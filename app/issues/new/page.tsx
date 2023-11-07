7164db5d6890709a9c599890a9467c3be691fef7
Adrià Ribas <ribasadria00@gmail.com>
2023-11-07 23:16:30 +0100

    implement new issue form client-side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,20 +2,28 @@
 
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-import { Button, TextField, Callout } from '@radix-ui/themes';
+import { Button, TextField, Callout, Text } from '@radix-ui/themes';
 import SimpleMdeReact from 'react-simplemde-editor';
 import { useForm, Controller, type SubmitHandler } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
 import axios from 'axios';
 import 'easymde/dist/easymde.min.css';
 
-type IssueForm = {
-  title: string;
-  description: string;
-};
+import { createIssueSchema } from '@/app/validationSchemas';
+
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage: React.FC = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const [error, setError] = useState('');
 
   const onSubmit: SubmitHandler<IssueForm> = async (data) => {
@@ -34,10 +42,17 @@ const NewIssuePage: React.FC = () => {
           <Callout.Text>{error}</Callout.Text>
         </Callout.Root>
       ) : null}
+
       <form onSubmit={handleSubmit(onSubmit)} className=' space-y-3'>
         <TextField.Root>
           <TextField.Input {...register('title')} placeholder='Title' />
         </TextField.Root>
+        {errors.title ? (
+          <Text as='p' color='red'>
+            {errors.title.message}
+          </Text>
+        ) : null}
+
         <Controller
           name='description'
           control={control}
@@ -45,6 +60,11 @@ const NewIssuePage: React.FC = () => {
             <SimpleMdeReact {...field} placeholder='Description' />
           )}
         />
+        {errors.description ? (
+          <Text as='p' color='red'>
+            {errors.description.message}
+          </Text>
+        ) : null}
 
         <Button type='submit'>Submit New Issue</Button>
       </form>
