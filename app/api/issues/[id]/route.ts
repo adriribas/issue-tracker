fe70f279177b17262d3a5176bc381337f95d0509
Adrià Ribas <ribasadria00@gmail.com>
2023-11-08 23:21:17 +0100

    add an API endpoint for deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,6 +1,6 @@
 import { issueSchema } from '@/app/validationSchemas';
 import prisma from '@/prisma/client';
-import { NextRequest, NextResponse } from 'next/server';
+import { type NextRequest, NextResponse } from 'next/server';
 
 export async function PATCH(
   request: NextRequest,
@@ -28,4 +28,20 @@ export async function PATCH(
   });
 
   return NextResponse.json(updatedIssue);
+}
+
+export async function DELETE(
+  _request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!issue) {
+    return NextResponse.json({ error: 'Invalid issue' }, { status: 404 });
+  }
+
+  await prisma.issue.delete({ where: { id: issue.id } });
+
+  return NextResponse.json({});
 }
