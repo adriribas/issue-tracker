4f4074407581d6e3832b871471d103e028c9695a
Adrià Ribas <ribasadria00@gmail.com>
2023-11-08 23:15:05 +0100

    show issue deletion confirmation box

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import type { Issue } from '@prisma/client';
-import { Button } from '@radix-ui/themes';
+import { AlertDialog, Button, Flex } from '@radix-ui/themes';
 import { TrashIcon } from '@radix-ui/react-icons';
 
 type Props = {
@@ -10,10 +10,33 @@ type Props = {
 
 const DeleteIssueButton: React.FC<Props> = ({ issueId }) => {
   return (
-    <Button color='red'>
-      <TrashIcon />
-      Delete Issue
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color='red'>
+          <TrashIcon />
+          Delete Issue
+        </Button>
+      </AlertDialog.Trigger>
+
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue? This action cannot be
+          undone.
+        </AlertDialog.Description>
+
+        <Flex className='gap-3 mt-4'>
+          <AlertDialog.Cancel>
+            <Button variant='soft' color='gray'>
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color='red'>Delete Issue</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
