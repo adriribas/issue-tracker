6bed3656eb394e73d40cb70cbb2fdc43d163b74d
Adrià Ribas <ribasadria00@gmail.com>
2023-11-08 23:34:14 +0100

    delete issues

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,14 +1,27 @@
 'use client';
 
+import { type MouseEventHandler } from 'react';
+import { useRouter } from 'next/navigation';
 import type { Issue } from '@prisma/client';
 import { AlertDialog, Button, Flex } from '@radix-ui/themes';
 import { TrashIcon } from '@radix-ui/react-icons';
+import axios from 'axios';
 
 type Props = {
   issueId: Issue['id'];
 };
 
 const DeleteIssueButton: React.FC<Props> = ({ issueId }) => {
+  const router = useRouter();
+
+  const handleClick: MouseEventHandler<HTMLButtonElement> = async () => {
+    try {
+      await axios.delete(`/api/issues/${issueId}`);
+      router.push('/issues');
+      router.refresh();
+    } catch (e) {}
+  };
+
   return (
     <AlertDialog.Root>
       <AlertDialog.Trigger>
@@ -32,7 +45,9 @@ const DeleteIssueButton: React.FC<Props> = ({ issueId }) => {
             </Button>
           </AlertDialog.Cancel>
           <AlertDialog.Action>
-            <Button color='red'>Delete Issue</Button>
+            <Button color='red' onClick={handleClick}>
+              Delete Issue
+            </Button>
           </AlertDialog.Action>
         </Flex>
       </AlertDialog.Content>
