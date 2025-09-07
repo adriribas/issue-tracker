a2494bb40c8fe0a49997a9848367a2722a6d1b31
Adrià Ribas <ribasadria00@gmail.com>
2025-09-07 12:53:03 +0200

    fix: initial migration

--- prisma/migrations/20231114232625_initial_migration/migration.sql
+++ prisma/migrations/20231114232625_initial_migration/migration.sql
@@ -40,17 +40,6 @@ CREATE TABLE `VerificationToken` (
     UNIQUE INDEX `VerificationToken_token_key`(`token`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-
--- CreateTable
-CREATE TABLE `VerificationToken` (
-    `identifier` VARCHAR(191) NOT NULL,
-    `token` VARCHAR(191) NOT NULL,
-    `expires` DATETIME(3) NOT NULL,
-
-    UNIQUE INDEX `VerificationToken_token_key`(`token`),
-    UNIQUE INDEX `VerificationToken_identifier_token_key`(`identifier`, `token`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
 -- CreateTable
 CREATE TABLE `Issue` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
